a1f6ff0c47251423b138f044e500b2cc2b75982c
Nang Tran Xuan <nang.tx194633@sis.hust.edu.vn>
2023-12-09 19:53:38 +0700

    add image seeder

---
+++ database/seeders/ImageSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i=1; $i <= 10; $i++) {
+            for($j=1; $j <= 4; $j++) {
+                DB::table('images')->insert(
+                [
+                    'post_id' => $i,
+                    'url' => "image\\$i\\anh$j.jpg"
+                ]);
+            }
+        }
+    }
+}
