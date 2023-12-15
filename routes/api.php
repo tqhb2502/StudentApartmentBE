75d1b0d0cfa9023c7ae75d505372d44691b72970
Nang Tran Xuan <nang.tx194633@sis.hust.edu.vn>
2023-12-15 22:21:06 +0700

    add: fn similar, filter

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ use App\Http\Controllers\PostController;
 Route::prefix('posts')->group(function () {
     Route::get('/', [PostController::class, 'index'])
         ->name('post.list');
-    Route::get('/aa', [PostController::class, 'index1'])
-        ->name('post.listaa');
+    Route::get('/similar', [PostController::class, 'similar'])
+        ->name('post.similar');
+    Route::get('/filter', [PostController::class, 'filter'])
+        ->name('post.filter');
 });
